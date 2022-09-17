fd13829757ccbba0243c38c92852f01811681167
Pieter Jordaan <pieter_jordaan@hotmail.com>
2022-09-17 14:29:24 +0200

    Added another form submit route definition for global actions, should still have a valid redirect

---
+++ packages/apie-bundle/tests/Cms/CmsGlobalMethodCommitTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Apie\Tests\ApieBundle\Cms;
+
+use Apie\Tests\ApieBundle\Concerns\ItCreatesASymfonyApplication;
+use Apie\Tests\ApieBundle\HtmlOutput;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class CmsGlobalMethodCommitTest extends TestCase
+{
+    use ItCreatesASymfonyApplication;
+
+    /**
+     * @test
+     */
+    public function it_can_submit_a_form_for_an_action(): void
+    {
+        $testItem = $this->given_a_symfony_application_with_apie();
+        $request = Request::create(
+            '/cms/default/action/ApplicationInfo/powerOf2',
+            'POST',
+            [],
+            [],
+            [],
+            [],
+            http_build_query(['form' => [
+                'input' => 42,
+            ]])
+        );
+        $response = $testItem->handle($request);
+        $this->assertEquals(301, $response->getStatusCode());
+        $this->assertEquals('http://localhost/cms/default/action/ApplicationInfo/powerOf2', $response->headers->get('Location'));
+    }
+}
