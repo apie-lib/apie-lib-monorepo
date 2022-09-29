0de6e4f5462be285c3c6513cb82db8e79fc370f9
Pieter Jordaan <pieter_jordaan@hotmail.com>
2022-09-29 16:53:47 +0200

    fake public properties in default behaviour

--- packages/apie-bundle/tests/BoundedContext/ValueObjects/CompositeObjectExample.php
+++ packages/apie-bundle/tests/BoundedContext/ValueObjects/CompositeObjectExample.php
@@ -2,11 +2,7 @@
 namespace Apie\Tests\ApieBundle\BoundedContext\ValueObjects;
 
 use Apie\CompositeValueObjects\CompositeValueObject;
-use Apie\Core\Identifiers\AutoIncrementInteger;
-use Apie\Core\Identifiers\IdentifierInterface;
 use Apie\Core\ValueObjects\Interfaces\ValueObjectInterface;
-use Apie\Tests\ApieBundle\BoundedContext\Entities\ManyColumns;
-use ReflectionClass;
 
 class CompositeObjectExample implements ValueObjectInterface
 {
