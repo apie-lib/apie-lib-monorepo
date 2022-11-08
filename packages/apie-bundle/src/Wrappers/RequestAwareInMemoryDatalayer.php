2bd4fb965bcfbd47e23a50af5e4bbf2e43de0c51
Pieter Jordaan <pieter_jordaan@hotmail.com>
2022-11-08 22:18:15 +0100

    Denormalize ids if we can also retrieve the linked entity in the data layer

--- packages/apie-bundle/src/Wrappers/RequestAwareInMemoryDatalayer.php
+++ packages/apie-bundle/src/Wrappers/RequestAwareInMemoryDatalayer.php
@@ -3,6 +3,7 @@ namespace Apie\ApieBundle\Wrappers;
 
 use Apie\Core\BoundedContext\BoundedContext;
 use Apie\Core\BoundedContext\BoundedContextId;
+use Apie\Core\Datalayers\ApieDatalayerWithSupport;
 use Apie\Core\Datalayers\BoundedContextAwareApieDatalayer;
 use Apie\Core\Datalayers\InMemory\InMemoryDatalayer;
 use Apie\Core\Datalayers\Lists\LazyLoadedList;
@@ -10,15 +11,28 @@ use Apie\Core\Entities\EntityInterface;
 use Apie\Core\Identifiers\IdentifierInterface;
 use ReflectionClass;
 
-final class RequestAwareInMemoryDatalayer implements BoundedContextAwareApieDatalayer
+final class RequestAwareInMemoryDatalayer implements BoundedContextAwareApieDatalayer, ApieDatalayerWithSupport
 {
     /**
      * @var array<string, InMemoryDatalayer>
      */
     private array $createdRepositories = [];
+
     public function __construct(
         private readonly BoundedContextSelected $boundedContextSelected
     ) {
+    }
+
+    public function isSupported(
+        EntityInterface|ReflectionClass|IdentifierInterface $instance,
+        BoundedContextId $boundedContextId
+    ): bool {
+        $className = ($instance instanceof ReflectionClass) ? $instance->name : get_class($instance);
+        if ($instance instanceof IdentifierInterface) {
+            $className = $instance->getReferenceFor()->name;
+        }
+        $boundedContext = $this->boundedContextSelected->getBoundedContextFromClassName($className);
+        return $boundedContext ? $boundedContext->getId()->toNative() === $boundedContextId->toNative() : false;
     }
 
     public function all(ReflectionClass $class, ?BoundedContext $boundedContext = null): LazyLoadedList
