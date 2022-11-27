f56ab56eaad98c73247dd831c7ac687651628788
Pieter Jordaan <pieter_jordaan@hotmail.com>
2022-11-27 16:50:05 +0100

    added extra phpstan rule and fixed a rule

--- packages/apie-bundle/tests/CreateResourceTest.php
+++ packages/apie-bundle/tests/CreateResourceTest.php
@@ -3,6 +3,7 @@ namespace Apie\Tests\ApieBundle;
 
 use Apie\Tests\ApieBundle\Concerns\ItCreatesASymfonyApplication;
 use Apie\Tests\ApieBundle\Concerns\ItValidatesOpenapi;
+use Generator;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -32,5 +33,35 @@ class CreateResourceTest extends TestCase
         $response = $testItem->handle($request);
         $this->validateResponse($request, $response);
         $this->assertEquals(201, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidDataProvider
+     */
+    public function it_can_throw_a_validation_error(array $input): void
+    {
+        $testItem = $this->given_a_symfony_application_with_apie();
+        $request = Request::create(
+            '/api/default/User',
+            'POST',
+            [],
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json', 'HTTP_ACCEPT' => 'application/json'],
+            json_encode($input)
+        );
+        $response = $testItem->handle($request);
+        $this->validateResponse($request, $response);
+        $this->assertEquals(422, $response->getStatusCode());
+    }
+
+    public function invalidDataProvider(): Generator
+    {
+        yield 'missing field and invalid value object' => [
+            [
+                'phoneNumber' => 'this is not a valid phone number',
+            ]
+        ];
     }
 }
