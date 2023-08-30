9b320a48365fddf798d3c685ff6db1deb6c33cf3
Pieter Jordaan <pieter_jordaan@hotmail.com>
2023-08-30 11:32:39 +0200

    initial commit recalculating idf

--- packages/doctrine-entity-datalayer/src/EntityReindexer.php
+++ packages/doctrine-entity-datalayer/src/EntityReindexer.php
@@ -7,6 +7,8 @@ use Apie\Core\Indexing\Indexer;
 use Apie\DoctrineEntityConverter\Interfaces\GeneratedDoctrineEntityInterface;
 use Apie\DoctrineEntityConverter\PropertyGenerators\ManyToEntityReferencePropertyGenerator;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\DBAL\Exception\DriverException;
+use Doctrine\ORM\Mapping\OneToMany;
 use LogicException;
 use ReflectionClass;
 use ReflectionProperty;
@@ -57,24 +59,60 @@ final class EntityReindexer
             $entity,
             new ApieContext()
         );
+        $termsToUpdate = array_keys($newIndexes);
         $offset = 0;
         $tf = 1.0 / count($newIndexes);
         foreach ($newIndexes as $text => $priority) {
             if (isset($currentIndex[$offset])) {
+                $termsToUpdate[] = $currentIndex[$offset]->text;
                 $currentIndex[$offset]->text = $text;
                 $currentIndex[$offset]->priority = $priority;
             } else {
                 $currentIndex[$offset] = $this->createIndexClass($doctrineEntity, $text, $priority);
                 $entityManager->persist($currentIndex[$offset]);
             }
-            $currentIndex[$offset]->tf = $tf;
+            $currentIndex[$offset]->tf = $tf * $priority;
             $offset++;
         }
         $count = count($currentIndex);
         for (;$offset < $count; $offset++) {
+            $termsToUpdate[] = $currentIndex[$offset]->text;
             $entityManager->remove($currentIndex[$offset]);
         }
         $doctrineEntity->_indexTable = $currentIndex;
         $entityManager->flush();
+        $this->recalculateIdf($doctrineEntity, $termsToUpdate);
+    }
+
+    private function recalculateIdf(GeneratedDoctrineEntityInterface $doctrineEntity, array $termsToUpdate): void
+    {
+        if (empty($termsToUpdate)) {
+            return;
+        }
+        $entityManager = $this->ormBuilder->createEntityManager();
+        $tableName = (new ReflectionClass($doctrineEntity))->getShortName();
+        $totalDocumentQuery = sprintf(
+            'SELECT total_documents FROM (SELECT COUNT(DISTINCT entity_id) AS total_documents FROM %s)',
+            $tableName
+        );
+        $documentWithTermQuery = sprintf(
+            'SELECT documents_with_term FROM (SELECT text, COUNT(DISTINCT entity_id) AS documents_with_term FROM %s GROUP BY text) AS sub WHERE sub.text = ',
+            $tableName
+        );
+        try {
+            $query = sprintf(
+                'UPDATE %s AS t
+            SET idf = LOG((%s))
+                - LOG(1 + (%s t.text))
+            WHERE t.text IN (?) AND EXISTS (SELECT 1 FROM (SELECT text, COUNT(DISTINCT entity_id) AS documents_with_term FROM %s GROUP BY text) AS sub WHERE sub.text = t.text);',
+                $tableName,
+                $totalDocumentQuery,
+                $documentWithTermQuery,
+                $tableName
+            );
+            $entityManager->getConnection()->prepare($query)->executeQuery([$termsToUpdate]);
+        } catch (DriverException) {
+            // TODO fallback
+        }
     }
 }
