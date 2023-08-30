9a359537fc5bb081ba8be1cc8861e30586daa89c
Pieter Jordaan <pieter_jordaan@hotmail.com>
2023-08-30 16:19:23 +0200

    PHP sqlite does not support LOG(), so we have a fallback to calculate IDF with sqlite

--- packages/doctrine-entity-datalayer/src/EntityReindexer.php
+++ packages/doctrine-entity-datalayer/src/EntityReindexer.php
@@ -7,7 +7,9 @@ use Apie\Core\Indexing\Indexer;
 use Apie\DoctrineEntityConverter\Interfaces\GeneratedDoctrineEntityInterface;
 use Apie\DoctrineEntityConverter\PropertyGenerators\ManyToEntityReferencePropertyGenerator;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\DBAL\ArrayParameterType;
 use Doctrine\DBAL\Exception\DriverException;
+use Doctrine\DBAL\ParameterType;
 use Doctrine\ORM\Mapping\OneToMany;
 use LogicException;
 use ReflectionClass;
@@ -27,17 +29,26 @@ final class EntityReindexer
      */
     private function createIndexClass(GeneratedDoctrineEntityInterface $doctrineEntity, string $text, float $priority): GeneratedDoctrineEntityInterface
     {
+        $className = $this->getIndexClass($doctrineEntity);
+        $res = new $className;
+        $res->text = $text;
+        $res->priority = $priority;
+        $res->entity = $doctrineEntity;
+        return $res;
+    }
+
+    /**
+     * @return class-string<GeneratedDoctrineEntityInterface>
+     */
+    private function getIndexClass(GeneratedDoctrineEntityInterface $doctrineEntity): string
+    {
         $refl = new ReflectionClass($doctrineEntity);
 
         $property = new ReflectionProperty($doctrineEntity, '_indexTable');
         $attributes = $property->getAttributes(OneToMany::class);
         foreach ($attributes as $attribute) {
             $className = $refl->getNamespaceName() . '\\' . $attribute->newInstance()->targetEntity;
-            $res = new $className;
-            $res->text = $text;
-            $res->priority = $priority;
-            $res->entity = $doctrineEntity;
-            return $res;
+            return $className;
         }
         throw new LogicException('The _indexTable property should have a OneToMany attribute');
     }
@@ -61,7 +72,7 @@ final class EntityReindexer
         );
         $termsToUpdate = array_keys($newIndexes);
         $offset = 0;
-        $tf = 1.0 / count($newIndexes);
+        $tf = 1.0 / array_sum($newIndexes);
         foreach ($newIndexes as $text => $priority) {
             if (isset($currentIndex[$offset])) {
                 $termsToUpdate[] = $currentIndex[$offset]->text;
@@ -90,29 +101,54 @@ final class EntityReindexer
             return;
         }
         $entityManager = $this->ormBuilder->createEntityManager();
-        $tableName = (new ReflectionClass($doctrineEntity))->getShortName();
+        $tableName = (new ReflectionClass($this->getIndexClass($doctrineEntity)))->getShortName();
         $totalDocumentQuery = sprintf(
             'SELECT total_documents FROM (SELECT COUNT(DISTINCT entity_id) AS total_documents FROM %s)',
             $tableName
         );
         $documentWithTermQuery = sprintf(
-            'SELECT documents_with_term FROM (SELECT text, COUNT(DISTINCT entity_id) AS documents_with_term FROM %s GROUP BY text) AS sub WHERE sub.text = ',
+            'SELECT documents_with_term FROM (SELECT text, COUNT(DISTINCT entity_id) AS documents_with_term FROM %s GROUP BY text) AS sub WHERE sub.text',
             $tableName
         );
         try {
             $query = sprintf(
                 'UPDATE %s AS t
-            SET idf = LOG((%s))
-                - LOG(1 + (%s t.text))
+            SET idf = LOG((%s)/(%s = t.text))
             WHERE t.text IN (?) AND EXISTS (SELECT 1 FROM (SELECT text, COUNT(DISTINCT entity_id) AS documents_with_term FROM %s GROUP BY text) AS sub WHERE sub.text = t.text);',
                 $tableName,
                 $totalDocumentQuery,
                 $documentWithTermQuery,
                 $tableName
             );
-            $entityManager->getConnection()->prepare($query)->executeQuery([$termsToUpdate]);
+            $entityManager->getConnection()
+                ->executeQuery(
+                    $query,
+                    [$termsToUpdate],
+                    [ArrayParameterType::STRING]
+                );
         } catch (DriverException) {
             // TODO fallback
+            $entityManager = $this->ormBuilder->createEntityManager();
+            $connection = $entityManager->getConnection();
+            $totalDocumentCount = $connection->prepare($totalDocumentQuery)->executeQuery()->fetchOne();
+            $documentTermCount = $connection->
+                executeQuery(
+                    str_replace('SELECT ', 'SELECT text, ', $documentWithTermQuery . ' IN (?)'),
+                    [$termsToUpdate],
+                    [ArrayParameterType::STRING],
+                )->fetchAllAssociativeIndexed();
+            foreach ($documentTermCount as $searchTerm => $rowResult) {
+                $idf = log10((1 + $totalDocumentCount) / (1 + $rowResult["documents_with_term"]));
+                $query = sprintf(
+                    'UPDATE %s SET idf = ? WHERE text = ?',
+                    $tableName
+                );
+                $connection->executeQuery(
+                    $query,
+                    [$idf, $searchTerm],
+                    [ParameterType::STRING, ParameterType::STRING]
+                );
+            }
         }
     }
 }
