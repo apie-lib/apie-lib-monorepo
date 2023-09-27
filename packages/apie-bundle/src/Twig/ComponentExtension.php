e7998c0036a9355119ec85e97df8a6ab8068d6c1
Pieter Jordaan <pieter_jordaan@hotmail.com>
2023-09-27 18:05:08 +0200

    render stacktrace in dev

--- packages/apie-bundle/src/Twig/ComponentExtension.php
+++ packages/apie-bundle/src/Twig/ComponentExtension.php
@@ -1,8 +1,10 @@
 <?php
 namespace Apie\ApieBundle\Twig;
 
+use Apie\HtmlBuilders\ErrorHandler\StacktraceRenderer;
 use Apie\HtmlBuilders\Interfaces\ComponentInterface;
 use Apie\HtmlBuilders\Interfaces\ComponentRendererInterface;
+use Throwable;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -16,7 +18,14 @@ class ComponentExtension extends AbstractExtension
     {
         return [
             new TwigFunction('renderApieComponent', [$this, 'renderApieComponent'], ['is_safe' => ['all']]),
+            new TwigFunction('renderStacktrace', [$this, 'renderStacktrace'], ['is_safe' => ['all']]),
         ];
+    }
+
+    public function renderStacktrace(Throwable $throwable): string
+    {
+        $renderer = new StacktraceRenderer($throwable);
+        return (string) $renderer;
     }
 
     public function renderApieComponent(ComponentInterface $component): string
