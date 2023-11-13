c5f6cd24ac6a4d106262d58588d15dc83833f5c6
Pieter Jordaan <pieter_jordaan@hotmail.com>
2023-11-13 06:35:05 +0100

    Add integration test for creating resource by console command

---
+++ packages/integration-tests/tests/Console/ApieCreateResourceCommandTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Apie\Tests\IntegrationTests\Console;
+
+use Apie\Core\BoundedContext\BoundedContextId;
+use Apie\IntegrationTests\Apie\TypeDemo\Resources\PrimitiveOnly;
+use Apie\IntegrationTests\Applications\Laravel\LaravelTestApplication;
+use Apie\IntegrationTests\IntegrationTestHelper;
+use Apie\IntegrationTests\Interfaces\TestApplicationInterface;
+use Apie\PhpunitMatrixDataProvider\MakeDataProviderMatrix;
+use Generator;
+use PHPUnit\Framework\TestCase;
+use ReflectionMethod;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+class ApieCreateResourceCommandTest extends TestCase
+{
+    use MakeDataProviderMatrix;
+
+    public function it_can_create_a_resource_provider(): Generator
+    {
+        yield from $this->createDataProviderFrom(
+            new ReflectionMethod($this, 'it_can_create_a_resource'),
+            new IntegrationTestHelper()
+        );
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @dataProvider it_can_create_a_resource_provider
+     * @test
+     */
+    public function it_can_create_a_resource(TestApplicationInterface $testApplication)
+    {
+        $testApplication->bootApplication();
+        $tester = new ApplicationTester($testApplication->getConsoleApplication());
+        $exitCode = $tester->run(['apie:types:create-PrimitiveOnly', '--input-id' => '075433c9-ca1f-435c-be81-61bae3009521']);
+        $this->assertEquals(Command::SUCCESS, $exitCode, 'console command gave me ' . $tester->getDisplay());
+        $this->assertGreaterThanOrEqual(
+            1,
+            $testApplication->getServiceContainer()->get('apie')->all(PrimitiveOnly::class, new BoundedContextId('types'))->getTotalCount()
+        );
+        $testApplication->cleanApplication();
+    }
+}
