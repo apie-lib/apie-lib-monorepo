e3ade0b32dffefaa9b4abde911e7b998dc2bd613
Pieter Jordaan <pieter_jordaan@hotmail.com>
2024-01-18 12:04:01 +0100

    Add @Route attribute for custom routes, allow object or EntityInterface as typehints for OpenAPI spec

--- packages/integration-tests/src/Apie/TypeDemo/Actions/Authentication.php
+++ packages/integration-tests/src/Apie/TypeDemo/Actions/Authentication.php
@@ -1,8 +1,12 @@
 <?php
 namespace Apie\IntegrationTests\Apie\TypeDemo\Actions;
 
+use Apie\Core\Attributes\Context;
+use Apie\Core\Attributes\Route;
 use Apie\Core\BoundedContext\BoundedContextId;
 use Apie\Core\Datalayers\ApieDatalayer;
+use Apie\Core\Entities\EntityInterface;
+use Apie\Core\Enums\RequestMethod;
 use Apie\IntegrationTests\Apie\TypeDemo\Identifiers\UserIdentifier;
 use Apie\IntegrationTests\Apie\TypeDemo\Resources\User;
 use Exception;
@@ -22,5 +26,12 @@ class Authentication
             return null;
         }
         return $user->verify($password) ? $user : null;
+    }
+
+    #[Route('/me', RequestMethod::GET)]
+    #[Route('/profile', target: Route::CMS)]
+    public function currentUser(#[Context('authenticated')] ?EntityInterface $currentUser = null): EntityInterface
+    {
+        return $currentUser;
     }
 }
