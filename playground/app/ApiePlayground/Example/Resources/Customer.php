443644bd1f1dd87783187ee7f535b70509b1393a
Pieter Jordaan <pieter_jordaan@hotmail.com>
2024-03-03 09:11:33 +0100

    Fix reindex query for MySQL

---
+++ playground/app/ApiePlayground/Example/Resources/Customer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\ApiePlayground\Example\Resources;
+
+use Apie\CountryAndPhoneNumber\DutchPhoneNumber;
+use App\ApiePlayground\Example\Identifiers\CustomerIdentifier;
+
+class Customer implements \Apie\Core\Entities\EntityInterface
+{
+    private CustomerIdentifier $id;
+
+    private ?DutchPhoneNumber $phoneNumber = null;
+
+    public function __construct()
+    {
+        $this->id = CustomerIdentifier::createRandom();
+    }
+
+    public function getId(): CustomerIdentifier
+    {
+        return $this->id;
+    }
+
+    public function setPhoneNumber(DutchPhoneNumber $phoneNumber): void
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    public function getPhoneNumber(): ?DutchPhoneNumber
+    {
+        return $this->phoneNumber;
+    }
+}
