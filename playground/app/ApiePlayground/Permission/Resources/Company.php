47147dca48a173f364365a1ff806d675b1d108ee
Pieter Jordaan <pieter_jordaan@hotmail.com>
2024-03-24 22:59:45 +0100

    add LoggedIn and HasRole examples in playground

--- playground/app/ApiePlayground/Permission/Resources/Company.php
+++ playground/app/ApiePlayground/Permission/Resources/Company.php
@@ -2,18 +2,32 @@
 
 namespace App\ApiePlayground\Permission\Resources;
 
+use Apie\Core\Attributes\HasRole;
 use Apie\Core\Attributes\LoggedIn;
 use Apie\Core\Attributes\RuntimeCheck;
+use Apie\TextValueObjects\CompanyName;
 use App\ApiePlayground\Permission\Identifiers\CompanyIdentifier;
 
+#[RuntimeCheck(new LoggedIn())]
 class Company implements \Apie\Core\Entities\EntityInterface
 {
     private CompanyIdentifier $id;
 
-    #[RuntimeCheck(new LoggedIn())]
-    public function __construct()
+    #[RuntimeCheck(new HasRole('ADMIN', 'MANAGER'))]
+    public function __construct(private CompanyName $companyName)
     {
         $this->id = CompanyIdentifier::createRandom();
+    }
+
+    #[RuntimeCheck(new HasRole('ADMIN', 'MANAGER'))]
+    public function setCompanyName(CompanyName $companyName)
+    {
+        $this->companyName = $companyName;
+    }
+
+    public function getCompanyName(): CompanyName
+    {
+        return $this->companyName;
     }
 
     public function getId(): CompanyIdentifier
--- playground/app/ApiePlayground/Permission/Resources/Customer.php
+++ playground/app/ApiePlayground/Permission/Resources/Customer.php
@@ -2,21 +2,32 @@
 
 namespace App\ApiePlayground\Permission\Resources;
 
+use Apie\Core\Attributes\Context;
 use Apie\Core\Attributes\LoggedIn;
 use Apie\Core\Attributes\RemovalCheck;
 use Apie\Core\Attributes\RuntimeCheck;
 use Apie\Core\Attributes\StaticCheck;
 use App\ApiePlayground\Permission\Identifiers\CustomerIdentifier;
+use App\ApiePlayground\Permission\Identifiers\UserIdentifier;
 
+#[RuntimeCheck(new LoggedIn())]
 #[RemovalCheck(new StaticCheck())]
 #[RemovalCheck(new RuntimeCheck(new LoggedIn()))]
 class Customer implements \Apie\Core\Entities\EntityInterface
 {
     private CustomerIdentifier $id;
+    private UserIdentifier $owner;
 
-    public function __construct()
-    {
+    public function __construct(
+        #[Context('authenticated')] User $user
+    ) {
         $this->id = CustomerIdentifier::createRandom();
+        $this->owner = $user->getId();
+    }
+
+    public function getOwner(): UserIdentifier
+    {
+        return $this->owner;
     }
 
     public function getId(): CustomerIdentifier
