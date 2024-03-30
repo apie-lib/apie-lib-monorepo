136dad8e60f4c69264fc561e43d693b90387be22
Pieter Jordaan <pieter_jordaan@hotmail.com>
2024-03-30 08:46:37 +0100

    Add SafeHtml value object for WYSIWYG fields, added AllowsLargeStringInterface marker interface for databases to know if they should larger text fields.

---
+++ playground/app/ApiePlayground/Types/Resources/StringFields.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\ApiePlayground\Types\Resources;
+
+use Apie\CommonValueObjects\Email;
+use Apie\CommonValueObjects\SafeHtml;
+use Apie\Core\Attributes\FakeCount;
+use Apie\Core\Lists\StringHashmap;
+use Apie\Core\Lists\StringList;
+use Apie\TextValueObjects\StrongPassword;
+use App\ApiePlayground\Types\Identifiers\StringFieldsIdentifier;
+
+#[FakeCount(25)]
+class StringFields implements \Apie\Core\Entities\EntityInterface
+{
+    private StringFieldsIdentifier $id;
+
+    public SafeHtml $safeHtml;
+
+    public string $regularString;
+
+    public ?string $nullableString;
+
+    public Email $email;
+
+    public StrongPassword $password;
+
+    public StringList $stringList;
+
+    public StringHashmap $stringHashmap;
+
+    public ?StringList $nullableStringList;
+
+    public ?StringHashmap $nullableStringHashmap;
+
+    public function __construct()
+    {
+        $this->id = StringFieldsIdentifier::createRandom();
+    }
+
+    public function getId(): StringFieldsIdentifier
+    {
+        return $this->id;
+    }
+}
