50113ba4704f4a79d22fa444a1ac4897719cb6e0
Pieter Jordaan <pieter_jordaan@hotmail.com>
2025-01-21 16:43:06 +0100

    Only run migrations when you connect to the database

--- packages/integration-tests/tests/DoctrineEntityDatalayer/DoctrineConsoleCommandTest.php
+++ packages/integration-tests/tests/DoctrineEntityDatalayer/DoctrineConsoleCommandTest.php
@@ -31,6 +31,27 @@ class DoctrineConsoleCommandTest extends TestCase
         );
     }
 
+    public static function it_can_run_cache_warmup_command_provider(): Generator
+    {
+        yield from self::createDataProviderFrom(
+            new ReflectionMethod(__CLASS__, 'it_can_run_doctrine_schema_validate_commands_on_apie_connection'),
+            new IntegrationTestHelper()
+        );
+    }
+
+    #[\PHPUnit\Framework\Attributes\DataProvider('it_can_run_cache_warmup_command_provider')]
+    #[\PHPUnit\Framework\Attributes\RunInSeparateProcess]
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_can_run_cache_warmup_command(
+        SymfonyTestApplication $testApplication
+    ) {
+        $testApplication->bootApplication();
+        $tester = new ApplicationTester($testApplication->getConsoleApplication());
+        $exitCode = $tester->run(['cache:warmup']);
+        $this->assertEquals(Command::SUCCESS, $exitCode, 'console command gave me ' . $tester->getDisplay());
+        $testApplication->cleanApplication();
+    }
+
     #[\PHPUnit\Framework\Attributes\DataProvider('it_can_run_doctrine_schema_update_commands_on_apie_connection_provider')]
     #[\PHPUnit\Framework\Attributes\RunInSeparateProcess]
     #[\PHPUnit\Framework\Attributes\Test]
@@ -52,7 +73,7 @@ class DoctrineConsoleCommandTest extends TestCase
     ) {
         $testApplication->bootApplication();
         $tester = new ApplicationTester($testApplication->getConsoleApplication());
-        $exitCode = $tester->run(['doctrine:schema:update', '--em' => MergedRegistry::APIE_MANAGER_NAME]);
+        $exitCode = $tester->run(['doctrine:schema:validate', '--em' => MergedRegistry::APIE_MANAGER_NAME]);
         $this->assertEquals(Command::SUCCESS, $exitCode, 'console command gave me ' . $tester->getDisplay());
         $testApplication->cleanApplication();
     }
