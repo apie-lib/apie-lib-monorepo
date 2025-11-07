35fe96ae1d1b2068dc04759aca39413f6c2762b7
Pieter Jordaan <pieter_jordaan@hotmail.com>
2025-11-07 07:48:18 +0100

    Add apie/ftp-server

--- packages/apie-file-system/src/ApieFilesystem.php
+++ packages/apie-file-system/src/ApieFilesystem.php
@@ -2,10 +2,39 @@
 namespace Apie\ApieFileSystem;
 
 use Apie\ApieFileSystem\Virtual\RootFolder;
+use Apie\ApieFileSystem\Virtual\VirtualFileInterface;
+use Apie\ApieFileSystem\Virtual\VirtualFolderInterface;
 
 class ApieFilesystem
 {
     public function __construct(public readonly RootFolder $rootFolder)
     {
+    }
+
+    public function visit(string $path): VirtualFolderInterface|VirtualFileInterface|null
+    {
+        $path = ltrim('/');
+        $current = $this->rootFolder;
+        $visited = [$current];
+        $paths = explode('/', $path);
+        while (!empty($paths)) {
+            $segment = array_shift($paths);
+            if ($segment === '.') {
+                continue;
+            }
+            if ($segment === '..') {
+                $current = array_pop($visited);
+                if (empty($visited)) {
+                    throw new \LogicException('Invalid path: ' . $path);
+                }
+                continue;
+            }
+            $current = $current->getChild($segment);
+            if ($current === null) {
+                return null;
+            }
+            $visited[] = $current;
+        }
+        return $current;
     }
 }
